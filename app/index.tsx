7a8f327fa7f56d96af92878653446baa4b573e22
ancung <ancungaulia@gmail.com>
2025-10-03 16:26:41 +0700

    Initial commit: setup project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-pink-300">
+      <Text className="">hydroconnect pelis aku butuh duit</Text>
+    </View>
+  );
+};
+
+export default index;
