3d82038f1fcb4c850c5f1df0279984e979ca873b
Troll321 <atila.ghulwani@gmail.com>
2025-10-28 23:31:28 +0700

    Change readings schema

--- schemas/readings.ts
+++ schemas/readings.ts
@@ -3,16 +3,18 @@ interface iReadings {
     pH: number;
     tds: number;
     temperature: number;
+    control: number;
     percent: number;
     timestamp: string;
 }
 
 const readingsSample: iReadings = {
-    percent: 0.5,
+    turbidity: 10,
     pH: 7,
     tds: 6,
     temperature: 10,
-    turbidity: 10,
+    control: 31,
+    percent: 0.5,
     timestamp: "2025-10-21T01:23:54.533+00:00",
 };
 
@@ -21,6 +23,7 @@ const readingsHeader: (keyof iReadings)[] = [
     "pH",
     "tds",
     "temperature",
+    "control",
     "percent",
     "timestamp",
 ];
