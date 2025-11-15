c55280d9e8c12ea049db81ac824381b3d6a71ff2
Troll321 <atila.ghulwani@gmail.com>
2025-11-15 22:17:28 +0700

    Add getLatestReading

--- lib/rest.ts
+++ lib/rest.ts
@@ -1,6 +1,7 @@
 import { type iSummaries } from "@/schemas/summaries";
 import { BACKEND_API_BASE_URL, BACKEND_API_REST_VERSION } from "./constants";
 import { HttpError } from "./errorHandler";
+import type { iReadings } from "@/schemas/readings";
 
 /**
  * @description Get last 7 days summaries
@@ -10,6 +11,25 @@ export async function getSummaries(): Promise<iSummaries[] | Error> {
     try {
         const result = await fetch(
             `${BACKEND_API_BASE_URL}/rest/${BACKEND_API_REST_VERSION}/summary`
+        );
+        if (result.status !== 200) {
+            throw "Connection Error";
+        }
+        const json = await result.json();
+        return json;
+    } catch {
+        return new HttpError(500);
+    }
+}
+
+/**
+ * @description Get latest readings
+ * @returns {Promise<iReadings | Error>}
+ */
+export async function getLatest(): Promise<iReadings | Error> {
+    try {
+        const result = await fetch(
+            `${BACKEND_API_BASE_URL}/rest/${BACKEND_API_REST_VERSION}/latest`
         );
         if (result.status !== 200) {
             throw "Connection Error";
