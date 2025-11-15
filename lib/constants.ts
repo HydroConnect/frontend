f4d170068537d41732cf8f701a5af2fe1ed234c6
Troll321 <atila.ghulwani@gmail.com>
2025-11-15 22:18:49 +0700

    Add constants and UI helper function (util.js)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,6 @@
-export const BACKEND_API_BASE_URL = "http://192.168.1.6:3000"; // On Development change this to server IP
+export const BACKEND_API_BASE_URL = "http://192.168.1.14:3000"; // On Development change this to server IP
 export const BACKEND_API_REST_VERSION = "v1";
 export const BACKEND_API_IO_VERSION = "v1";
 export const MAX_DOWNLOAD_ID_LENGTH = 10;
+export const ON_OFF_THRESHOLD_MS = 6000;
+export const SUMMARY_GRAPH_PRECISION = 2;
---
+++ lib/utils.ts
@@ -0,0 +1,39 @@
+export function formatDate(date: Date): string {
+    return new Intl.DateTimeFormat("id-ID", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(date);
+}
+
+export function getHari(date: Date): string {
+    return new Intl.DateTimeFormat("id-ID", {
+        weekday: "long",
+    }).format(date);
+}
+
+export function getJam(date: Date): string {
+    return new Intl.DateTimeFormat("id-ID", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+    })
+        .format(date)
+        .replace(":", ".");
+}
+
+export function debounce(state: any, fun: (...args: any[]) => any, timeout: number = 400): void {
+    if (state.now === true) {
+        return;
+    }
+    fun();
+    state.now = true;
+    setTimeout(() => {
+        state.now = false;
+    }, timeout);
+}
+
+export function round(num: number, precision: number) {
+    return Math.round(num * 10 ** precision) / 10 ** precision;
+}
