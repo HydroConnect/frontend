6db1016412e6e1da6eab993e08cd8334d9dc3bdc
Troll321 <atila.ghulwani@gmail.com>
2025-11-18 13:38:49 +0700

    Add IOT_INTERVAL_MS

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,8 @@
-export const BACKEND_API_BASE_URL = "http://192.168.1.13:3000"; // On Development change this to server IP
+export const BACKEND_API_BASE_URL = "http://192.168.1.12:3000"; // On Development change this to server IP
 export const BACKEND_API_REST_VERSION = "v1";
 export const BACKEND_API_IO_VERSION = "v1";
 export const MAX_DOWNLOAD_ID_LENGTH = 10;
 export const ON_OFF_THRESHOLD_MS = 6000;
 export const FETCH_REST_TIME_MS = 10000;
+export const IOT_INTERVAL_MS = 2000;
 export const SUMMARY_GRAPH_PRECISION = 2;
