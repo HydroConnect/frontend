ec78ae1822d9d0b1f6c43db3760a471972d422db
Troll321 <atila.ghulwani@gmail.com>
2025-12-18 12:18:21 +0700

    Integrate Notification API to APITEST

--- app/ApiTest.tsx
+++ app/ApiTest.tsx
@@ -7,17 +7,25 @@ import { type iSummaries } from "@/schemas/summaries";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Button, FlatList, ScrollView, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
-import { registerForPushNotificationsAsync, sendPushNotification } from "@/lib/notifications";
+import {
+    getNotifications,
+    registerForPushNotificationsAsync,
+    saveNotification,
+    sendPushNotification,
+} from "@/lib/notifications";
+import { usageNotificationSample, type iUsageNotification } from "@/schemas/usageNotification";
 
 const ApiTest = () => {
     const [log, setLog] = useState<any>();
     const [summary, setSummary] = useState<iSummaries[] | null>(null);
     const [expoPushToken, setExpoPushToken] = useState("");
+    const [latestNPointer, setLatestNPointer] = useState<number | null>(null);
     const [notification, setNotification] = useState<Notifications.Notification | undefined>(
         undefined
     );
+    const [myNotifs, setMyNotifs] = useState<iUsageNotification[]>([]);
 
     function print(input: any) {
         console.log(input);
@@ -48,138 +56,172 @@ const ApiTest = () => {
     }, []);
 
     return (
-        <View className="flex w-[100%] h-[100%] relative justify-center items-center">
-            <ScrollView
-                contentContainerStyle={{
-                    position: "relative",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    display: "flex",
-                }}>
-                <View>
-                    <Link href={"/"} push asChild>
-                        <Text className="font-bold">Back</Text>
-                    </Link>
-                    <Text>{log}</Text>
-                </View>
-                <View className="h-56 w-max bg-slate-50">
-                    <Button
-                        title="GET /summary"
-                        onPress={() => {
-                            getSummaries().then((data) => {
-                                if (data instanceof Error) {
-                                    print(data);
-                                    return;
-                                }
-                                setSummary(data);
-                            });
-                        }}
-                    />
-                    <Button
-                        title="GET /latest"
-                        onPress={() => {
-                            getLatest().then((data) => {
-                                if (data instanceof Error) {
-                                    print(data);
-                                    return;
-                                }
+        // <View className="flex w-[100%] h-[100%] relative justify-center items-center">
+        <ScrollView
+            style={{
+                position: "relative",
+                // justifyContent: "center",
+                // alignItems: "center",
+                // display: "flex",
+                // flex: 0,
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "80%",
+            }}>
+            <View className="relative h-10"></View>
+            <View>
+                <Link href={"/"} push asChild>
+                    <Text className="font-bold">Back</Text>
+                </Link>
+                <Text>{log}</Text>
+            </View>
+            <View className="mb-5 h-56 w-max bg-slate-200">
+                <Button
+                    title="GET /summary"
+                    onPress={() => {
+                        getSummaries().then((data) => {
+                            if (data instanceof Error) {
                                 print(data);
-                            });
-                        }}
-                    />
-                    <FlatList
-                        className="h-40"
-                        data={summary ?? []}
-                        renderItem={({ item }) => {
-                            return <Text className="text-black">Data at {item.timestamp}</Text>;
-                        }}
-                    />
-                </View>
-                <View className="h-56 w-max bg-slate-200">
-                    <Button
-                        title="IO /listen"
-                        onPress={() => {
-                            connectAndListen(
-                                () => {
-                                    console.log("Connected");
-                                },
-                                () => {
-                                    console.log("Connection lost!");
-                                },
-                                (readings: iReadings) => {
-                                    print(readings);
-                                }
-                            );
-                        }}
-                    />
-                    <Button
-                        title="IO /disconnect"
-                        onPress={() => {
-                            disconnect();
-                        }}
-                    />
-                    <Button
-                        title="Check connection"
-                        onPress={() => {
-                            print(isConnected());
-                        }}
-                    />
-                </View>
-                <View className="h-56 w-max bg-slate-300">
-                    <Button
-                        title="IO /download-request"
-                        onPress={async () => {
-                            const hasil = await downloadReports(downloadRequestSample, () => {});
-                            if (hasil instanceof Error) {
-                                console.log(hasil);
+                                return;
                             }
-                        }}
-                    />
-                    <Button
-                        title="IO /resume-request"
-                        onPress={async () => {
-                            const hasil = await resumeDownload(() => {});
-                            if (hasil instanceof Error) {
-                                console.log(hasil);
+                            setSummary(data);
+                        });
+                    }}
+                />
+                <Button
+                    title="GET /latest"
+                    onPress={() => {
+                        getLatest().then((data) => {
+                            if (data instanceof Error) {
+                                print(data);
+                                return;
                             }
-                        }}
-                    />
-                    <Button
-                        title="IO /resume-request (_forcePick)"
-                        onPress={async () => {
-                            const hasil = await resumeDownload(() => {}, true);
-                            if (hasil instanceof Error) {
-                                console.log(hasil);
+                            print(data);
+                        });
+                    }}
+                />
+                <View className="bg-slate-200 h-40">
+                    {(summary ?? []).map((item, index) => {
+                        return (
+                            <Text key={index} className="text-black">
+                                Data at {item.timestamp}
+                            </Text>
+                        );
+                    })}
+                </View>
+            </View>
+            <View className="mb-5 h-56 w-max bg-slate-200">
+                <Button
+                    title="IO /listen"
+                    onPress={() => {
+                        connectAndListen(
+                            () => {
+                                console.log("Connected");
+                            },
+                            () => {
+                                console.log("Connection lost!");
+                            },
+                            (readings: iReadings) => {
+                                print(readings);
                             }
-                        }}
-                    />
-                    <Button
-                        title="IO /reset"
-                        onPress={async () => {
-                            await AsyncStorage.clear();
-                            console.log("RESET!");
-                        }}
-                    />
+                        );
+                    }}
+                />
+                <Button
+                    title="IO /disconnect"
+                    onPress={() => {
+                        disconnect();
+                    }}
+                />
+                <Button
+                    title="Check connection"
+                    onPress={() => {
+                        print(isConnected());
+                    }}
+                />
+            </View>
+            <View className="mb-5 h-56 w-max bg-slate-200">
+                <Button
+                    title="IO /download-request"
+                    onPress={async () => {
+                        const hasil = await downloadReports(downloadRequestSample, () => {});
+                        if (hasil instanceof Error) {
+                            console.log(hasil);
+                        }
+                    }}
+                />
+                <Button
+                    title="IO /resume-request"
+                    onPress={async () => {
+                        const hasil = await resumeDownload(() => {});
+                        if (hasil instanceof Error) {
+                            console.log(hasil);
+                        }
+                    }}
+                />
+                <Button
+                    title="IO /resume-request (_forcePick)"
+                    onPress={async () => {
+                        const hasil = await resumeDownload(() => {}, true);
+                        if (hasil instanceof Error) {
+                            console.log(hasil);
+                        }
+                    }}
+                />
+                <Button
+                    title="IO /reset"
+                    onPress={async () => {
+                        await AsyncStorage.clear();
+                        console.log("RESET!");
+                    }}
+                />
+            </View>
+            <View className="mb-5 h-56 w-max bg-slate-200">
+                <Text>Your Expo push token: {expoPushToken}</Text>
+                <Button
+                    title="Press to Send Notification"
+                    onPress={async () => {
+                        await sendPushNotification(expoPushToken);
+                    }}
+                />
+                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                    <Text>Title: {notification && notification.request.content.title} </Text>
+                    <Text>Body: {notification && notification.request.content.body}</Text>
+                    <Text>
+                        Data: {notification && JSON.stringify(notification.request.content.data)}
+                    </Text>
                 </View>
-                <View style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}>
-                    <Text>Your Expo push token: {expoPushToken}</Text>
-                    <View style={{ alignItems: "center", justifyContent: "center" }}>
-                        <Text>Title: {notification && notification.request.content.title} </Text>
-                        <Text>Body: {notification && notification.request.content.body}</Text>
-                        <Text>
-                            Data:{" "}
-                            {notification && JSON.stringify(notification.request.content.data)}
-                        </Text>
-                    </View>
-                    <Button
-                        title="Press to Send Notification"
-                        onPress={async () => {
-                            await sendPushNotification(expoPushToken);
-                        }}
-                    />
+
+                <Button
+                    title="Get Notification (Latest)"
+                    onPress={async () => {
+                        const [nowNotifs, nowPointer] = await getNotifications(latestNPointer);
+                        setMyNotifs(nowNotifs);
+                        setLatestNPointer(nowPointer);
+                    }}
+                />
+                <Button
+                    title="Reset Latest"
+                    onPress={async () => {
+                        setLatestNPointer(null);
+                    }}
+                />
+                <Button
+                    title="Save Notification"
+                    onPress={async () => {
+                        await saveNotification(usageNotificationSample);
+                    }}
+                />
+                <Text>{latestNPointer}</Text>
+                <View>
+                    {/* {myNotifs.map((val, index) => {
+                        return <></>;
+                    })} */}
                 </View>
-            </ScrollView>
-        </View>
+            </View>
+            <View className="relative h-20"></View>
+        </ScrollView>
+        // </View>
     );
 };
 
